aa7c1ed156de6957c065f55d1dafe8ee0a70b219
SanChe (AWS DeVops) <132386474+SanjayGit2023@users.noreply.github.com>
2024-06-19 19:36:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World Cloud guru!');
+    res.send('Hello World Cloud gurus!');
 });
 
 app.listen(port, () => {
